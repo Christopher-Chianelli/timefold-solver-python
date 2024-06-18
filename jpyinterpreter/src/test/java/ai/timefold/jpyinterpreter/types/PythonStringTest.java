b51db1ce69acebe89e75d3a9b7b1d1ff2402e3e9
Christopher Chianelli <christopher@timefold.ai>
2024-06-18 02:08:27 -0400

    chore: remove unused import

--- jpyinterpreter/src/test/java/ai/timefold/jpyinterpreter/types/PythonStringTest.java
+++ jpyinterpreter/src/test/java/ai/timefold/jpyinterpreter/types/PythonStringTest.java
@@ -1,7 +1,6 @@
 package ai.timefold.jpyinterpreter.types;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
